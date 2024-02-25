0195204e1bc7b0b2463133a6ac14723c688a7d9b
Jar <123274988+FajarKyy@users.noreply.github.com>
2024-02-25 11:49:56 +0700

    Delete data/Authorr.py

--- data/Authorr.py
+++
@@ -1,5 +0,0 @@
-from rich.console import Console as _Bapaku_Garena_Bapamu_Siapa___
-Auth = {'Author' : 'Fajar Khafi'}#> Jangan Di Ganti Ya Kontol
-def Authorr():
-        if Auth['Author'] == 'Fajar Khafi':pass
-        else:_Bapaku_Garena_Bapamu_Siapa___().print('[[bold red]°[bol>
