6b1fed13f444a6c4d7e8a2346cace9f1f2d65458
Jar <123274988+FajarKyy@users.noreply.github.com>
2024-02-25 11:55:00 +0700

    Create Authorr.py

---
+++ data/Authorr.py
@@ -0,0 +1,6 @@
+#Hayo Mau Recode ya
+from rich.console import Console as _Bapaku_Garena_Bapamu_Siapa___
+Auth = {'Author' : 'Fajar Khafi'}#> Jangan Di Ganti Ya Kontol 
+def Authorr():
+	if Auth['Author'] == 'Fajar Khafi':pass
+	else:_Bapaku_Garena_Bapamu_Siapa___().print('[[bold red]°[bold white]] [bold red]Anak Anjing di bilang Jangan Recode[bold white]\n[[bold red]°[bold white]] Author: Fajar Khafi\n[[bold red]°[bold white]] Hargai Yang buat :(');exit()
