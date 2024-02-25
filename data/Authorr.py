7ec253bc09e5d6a6e6c05f763d051be4b5a76da1
Jar <123274988+FajarKyy@users.noreply.github.com>
2024-02-25 11:49:36 +0700

    data

---
+++ data/Authorr.py
@@ -0,0 +1,5 @@
+from rich.console import Console as _Bapaku_Garena_Bapamu_Siapa___
+Auth = {'Author' : 'Fajar Khafi'}#> Jangan Di Ganti Ya Kontol
+def Authorr():
+        if Auth['Author'] == 'Fajar Khafi':pass
+        else:_Bapaku_Garena_Bapamu_Siapa___().print('[[bold red]°[bol>
